be9f392f452712fe5f909e73ca092857ba4a91d4
Nat Budin <natbudin@gmail.com>
2008-05-05 19:19:46 +0000

    Fix issue 22: show questionnaire titles as "Untitled" if they are blank

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -67,4 +67,12 @@ module ApplicationHelper
       'link'
     end
   end
+  
+  def format_title(str)
+    ret = str.to_s
+    if ret.nil? or ret.length == 0 or ret.strip.length == 0
+      ret = "Untitled"
+    end
+    return ret
+  end
 end

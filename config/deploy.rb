a5b323d8c5ab9faf53694de3f169bd069af3fdf4
Nat Budin <natbudin@gmail.com>
2009-05-18 14:52:03 -0400

    Use the new standard deployment recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,61 +1,21 @@
-# This defines a deployment "recipe" that you can feed to capistrano
-# (http://manuals.rubyonrails.com/read/book/17). It allows you to automate
-# (among other things) the deployment of your application.
+require 'capistrano/ext/multistage'
 
 set :stages, %w{trunk}
 set :default_stage, "trunk"
-require 'capistrano/ext/multistage'
-
-# =============================================================================
-# REQUIRED VARIABLES
-# =============================================================================
-# You must always specify the application and repository for every recipe. The
-# repository must be the URL of the repository you want this recipe to
-# correspond to. The deploy_to path must be the path on each machine that will
-# form the root of the application path.
 
 set :application, "journey"
-set :repository, "http://journey-questionnaires.googlecode.com/svn/branches/2.5"
-
-# =============================================================================
-# ROLES
-# =============================================================================
-# You can define any number of roles, each of which contains any number of
-# machines. Roles might include such things as :web, or :app, or :db, defining
-# what the purpose of each machine is. You can also specify options that can
-# be used to single out a specific subset of boxes in a particular role, like
-# :primary => true.
+set :repository, "git://github.com/nbudin/journey.git"
 
 role :web, "sakai.natbudin.com"
 role :app, "sakai.natbudin.com"
-role :db,  "sakai.natbudin.com", :primary => true
+role :db,  "ishinabe.natbudin.com", :primary => true
 
-# =============================================================================
-# OPTIONAL VARIABLES
-# =============================================================================
-#set :use_sudo, true
 set :checkout, "export"
-set :user, "www-data"            # defaults to the currently logged in user
-#set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
-set :scm, :subversion               # defaults to :subversion
-# set :svn, "/path/to/svn"       # defaults to searching the PATH
-# set :darcs, "/path/to/darcs"   # defaults to searching the PATH
-# set :cvs, "/path/to/cvs"       # defaults to searching the PATH
-# set :gateway, "gate.host.com"  # default to no gateway
-
-# =============================================================================
-# SSH OPTIONS
-# =============================================================================
-# ssh_options[:keys] = %w(/path/to/my/key /path/to/another/key)
-# ssh_options[:port] = 25
-
-# =============================================================================
-# TASKS
-# =============================================================================
-# Define tasks that run on all (or only some) of the machines. You can specify
-# a role (or set of roles) that each task should be executed on. You can also
-# narrow the set of servers to a subset of a role by specifying options, which
-# must match the options given for the servers to select (like :primary => true)
+set :user, "www-data"
+set :scm, :git
+set :use_sudo, false
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1
 
 namespace :deploy do
   desc "Tell Passenger to restart this app"
@@ -69,7 +29,6 @@ namespace :deploy do
     run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
     imagesdir = "#{deploy_to}/#{shared_dir}/public/images"
     run "for f in #{imagesdir}/*; do ln -nfs $f #{release_path}/public/images/; done"
-  #  run "cd #{release_path} ; rake bootstrap RAILS_ENV=production"
   end
 end
 

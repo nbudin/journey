72bf13d0e7956e594f89c358662ebd83a4698335
Nat Budin <nbudin@Kenichi.(none)>
2009-05-31 23:12:44 -0400

    Separating out the paywall stuff into a plugin

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090524174411) do
+ActiveRecord::Schema.define(:version => 20090523171744) do
 
   create_table "answers", :force => true do |t|
     t.integer  "response_id"
@@ -22,14 +22,14 @@ ActiveRecord::Schema.define(:version => 20090524174411) do
   add_index "answers", ["response_id"], :name => "index_answers_on_response_id"
 
   create_table "auth_tickets", :force => true do |t|
-    t.string   "secret"
+    t.string   "secret",     :limit => 40
     t.integer  "person_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "expires_at"
   end
 
-  add_index "auth_tickets", ["secret"], :name => "index_auth_tickets_on_secret", :unique => true
+  add_index "auth_tickets", ["secret"], :name => "secret", :unique => true
 
   create_table "characters", :force => true do |t|
     t.string  "name"
@@ -42,17 +42,10 @@ ActiveRecord::Schema.define(:version => 20090524174411) do
     t.string  "path"
   end
 
-  create_table "entitlements", :force => true do |t|
-    t.integer  "person_id"
-    t.boolean  "unlimited"
-    t.datetime "expires_at"
-    t.integer  "open_questionnaires"
-    t.integer  "responses_per_month"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "engine_schema_info", :id => false, :force => true do |t|
+    t.string  "engine_name"
+    t.integer "version"
   end
-
-  add_index "entitlements", ["person_id"], :name => "index_entitlements_on_person_id"
 
   create_table "larp_runs", :force => true do |t|
     t.integer  "larp_id"
@@ -85,17 +78,34 @@ ActiveRecord::Schema.define(:version => 20090524174411) do
   end
 
   create_table "pages", :force => true do |t|
-    t.integer "questionnaire_id", :null => false
+    t.integer "questionnaire_id", :default => 0, :null => false
     t.integer "position"
     t.string  "title"
   end
 
+  create_table "permission_caches", :force => true do |t|
+    t.integer "person_id"
+    t.integer "permissioned_id"
+    t.string  "permissioned_type"
+    t.string  "permission_name"
+    t.boolean "result"
+  end
+
+  add_index "permission_caches", ["permission_name"], :name => "index_permission_caches_on_permission_name"
+  add_index "permission_caches", ["permissioned_id", "permissioned_type"], :name => "index_permission_caches_on_permissioned"
+  add_index "permission_caches", ["person_id"], :name => "index_permission_caches_on_person_id"
+
   create_table "permissions", :force => true do |t|
     t.integer "role_id"
-    t.integer "person_id"
     t.string  "permission"
     t.integer "permissioned_id"
     t.string  "permissioned_type"
+    t.integer "person_id"
+  end
+
+  create_table "permissions_roles", :id => false, :force => true do |t|
+    t.integer "permission_id", :default => 0, :null => false
+    t.integer "role_id",       :default => 0, :null => false
   end
 
   create_table "players", :id => false, :force => true do |t|
@@ -105,6 +115,11 @@ ActiveRecord::Schema.define(:version => 20090524174411) do
 
   add_index "players", ["larp_run_id", "user_id"], :name => "index_players_on_larp_run_id_and_user_id", :unique => true
 
+  create_table "plugin_schema_info", :id => false, :force => true do |t|
+    t.string  "plugin_name"
+    t.integer "version"
+  end
+
   create_table "projects", :force => true do |t|
     t.string "name",     :null => false
     t.string "repo_url", :null => false
@@ -113,17 +128,17 @@ ActiveRecord::Schema.define(:version => 20090524174411) do
   end
 
   create_table "question_options", :force => true do |t|
-    t.integer "question_id",  :null => false
-    t.text    "option",       :null => false
-    t.integer "position",     :null => false
+    t.integer "question_id",  :default => 0, :null => false
+    t.text    "option",                      :null => false
+    t.integer "position"
     t.string  "output_value"
   end
 
   create_table "questionnaires", :force => true do |t|
     t.text     "title"
     t.boolean  "is_open"
-    t.string   "custom_html",          :default => ""
-    t.string   "custom_css",           :default => ""
+    t.text     "custom_html"
+    t.text     "custom_css"
     t.boolean  "allow_finish_later",   :default => true,  :null => false
     t.boolean  "allow_amend_response", :default => true,  :null => false
     t.string   "rss_secret"
@@ -132,19 +147,21 @@ ActiveRecord::Schema.define(:version => 20090524174411) do
     t.datetime "updated_at"
     t.boolean  "advertise_login",      :default => true
     t.boolean  "require_login",        :default => false
-    t.integer  "owner_id"
+    t.integer  "subscription_id"
   end
+
+  add_index "questionnaires", ["subscription_id"], :name => "index_questionnaires_on_subscription_id"
 
   create_table "questions", :force => true do |t|
     t.string  "type",           :limit => 100, :default => "",     :null => false
-    t.integer "position",                      :default => 0,      :null => false
+    t.integer "position"
     t.text    "caption",                                           :null => false
     t.boolean "required",                      :default => false,  :null => false
     t.integer "min",                           :default => 0,      :null => false
     t.integer "max",                           :default => 0,      :null => false
     t.integer "step",                          :default => 1,      :null => false
-    t.integer "page_id",                                           :null => false
-    t.string  "default_answer"
+    t.integer "page_id",                       :default => 0,      :null => false
+    t.text    "default_answer"
     t.string  "layout",                        :default => "left"
   end
 
@@ -161,10 +178,28 @@ ActiveRecord::Schema.define(:version => 20090524174411) do
     t.datetime "submitted_at"
   end
 
+  add_index "responses", ["questionnaire_id"], :name => "index_responses_on_questionnaire_id"
+
+  create_table "roles", :force => true do |t|
+    t.string  "name",        :default => "",    :null => false
+    t.string  "description"
+    t.boolean "omnipotent",  :default => false, :null => false
+    t.boolean "system_role", :default => false, :null => false
+  end
+
   create_table "special_field_associations", :force => true do |t|
     t.integer "questionnaire_id"
     t.integer "question_id"
     t.string  "purpose"
+  end
+
+  create_table "subscriptions", :force => true do |t|
+    t.boolean  "unlimited"
+    t.datetime "expires_at"
+    t.integer  "open_questionnaires"
+    t.integer  "responses_per_month"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "taggings", :force => true do |t|
@@ -185,5 +220,28 @@ ActiveRecord::Schema.define(:version => 20090524174411) do
   end
 
   add_index "tags", ["name"], :name => "index_tags_on_name", :unique => true
+
+  create_table "users", :force => true do |t|
+    t.string   "login",           :limit => 80, :default => "", :null => false
+    t.string   "salted_password", :limit => 40, :default => "", :null => false
+    t.string   "email",           :limit => 60, :default => "", :null => false
+    t.string   "firstname",       :limit => 40
+    t.string   "lastname",        :limit => 40
+    t.string   "salt",            :limit => 40, :default => "", :null => false
+    t.integer  "verified",                      :default => 0
+    t.string   "role",            :limit => 40
+    t.string   "security_token",  :limit => 40
+    t.datetime "token_expiry"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "logged_in_at"
+    t.integer  "deleted",                       :default => 0
+    t.datetime "delete_after"
+  end
+
+  create_table "users_roles", :id => false, :force => true do |t|
+    t.integer "user_id", :default => 0, :null => false
+    t.integer "role_id", :default => 0, :null => false
+  end
 
 end

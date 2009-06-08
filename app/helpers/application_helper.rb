b8bc569cf98abb43f56bda07d18d99ad1b237ba5
Nat Budin <natbudin@gmail.com>
2009-06-08 17:23:20 -0400

    Tabby goodness, mmm

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,11 +20,6 @@ module ApplicationHelper
     end
   end
 
-  def icon_for(record_or_class)
-    klass = SimplyHelpful::RecordIdentifier::singular_class_name(record_or_class)
-    image_tag "icons/#{klass}.png", :alt => klass.humanize, :class => 'icon'
-  end
-
   def create_form_dom_id(klass)
     "#{dom_id(klass)}_create_form"
   end
@@ -100,6 +95,40 @@ module ApplicationHelper
       end
     end
     return 'link'
+  end
+  
+  def tabstrip_li(caption, url, options={})
+    level = options.delete(:level) || 1
+    classes = []
+    classes.push(options.delete(:class) || 'link')
+    
+    if level == 1
+      ctrlr = options.delete :controller
+      if params[:controller] == ctrlr
+        except = options.delete :except
+        if except.nil? or (not except.include?(params[:action]))
+          only = options.delete :only
+          if only.nil? or (only.include?(params[:action]))
+            classes.push 'selected'
+          end
+        end
+      end
+    elsif level == 2
+      if params[:action] == options.delete(:action)
+        classes.push 'selected'
+      end
+    end
+    
+    if options.delete(:disabled)
+      classes.push 'disabled'
+    end
+    
+    content_tag("li", options.update(:class => classes.join(" "))) do
+      if classes.include? 'disabled' or classes.include? 'selected'
+        url = "#"
+      end
+      link_to(caption, url)
+    end
   end
   
   def user_options

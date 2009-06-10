ecad0c801d40c9665c800b2581190abf107b4cfb
Nat Budin <natbudin@gmail.com>
2009-06-10 16:58:11 -0400

    Replace Scout with NewRelic

--- vendor/plugins/scout_rails_instrumentation/init.rb
+++
@@ -1,9 +0,0 @@
-require 'scout/rails'
-Scout.start! do
-  ActionController::Base.class_eval do
-    alias_method_chain :perform_action, :instrumentation
-  end
-  ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
-    alias_method_chain :log, :instrumentation
-  end
-end

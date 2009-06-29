4a2a6940f6bc0d196254cebead858c9e66d328f1
Nat Budin <natbudin@gmail.com>
2009-06-29 10:19:04 -0400

    Prettify the question dropdown a bit

--- app/helpers/responses_helper.rb
+++ app/helpers/responses_helper.rb
@@ -1,22 +1,46 @@
 module ResponsesHelper
   def column_selector_option(caption, id, selected_id, attrs={})
     attrs[:value] = id
-    if id == selected_id
+    if selected_id and id == selected_id
       attrs[:selected] = "selected"
     end
     
     content_tag(:option, caption, attrs)
   end
   
+  def column_selector_header_option(caption)
+    column_selector_option(caption, "", nil, :disabled => "disabled",
+                           :style => "font-weight: bold; border-bottom: 1px black dotted;")
+  end
+  
+  def column_selector_divider_option
+    column_selector_option("", "", nil, :disabled => "disabled")
+  end
+  
+  def column_selector_question_option(question, selected)
+    column_selector_option(truncate(question.caption), "question_#{question.id}", selected, 
+                           :style => "padding-left: 1em;")
+  end
+  
+  def column_selector_meta_option(caption, id, selected)
+    column_selector_option(caption, id, selected, :style => "font-style: italic; padding-left: 1em;")
+  end
+  
   def column_selector_options(questionnaire, selected)
-    meta_option_style = "font-style: italic;"
-    
-    options = [column_selector_option("Title", "title", selected, :style => meta_option_style),
-               column_selector_option("Submitted at", "submitted_at", selected, :style => meta_option_style)]
-    options += questionnaire.fields.collect do |f|
-      column_selector_option(truncate(f.caption), "question_#{f.id}", selected)
+    options = [column_selector_header_option("General response fields"),
+               column_selector_meta_option("Title", "title", selected),
+               column_selector_meta_option("Submitted at", "submitted_at", selected),
+               column_selector_meta_option("Response ID", "id", selected)]
+    questionnaire.pages.each do |page|
+      fields = page.fields
+      if page.fields.size > 0
+        options.push(column_selector_divider_option)
+        options.push(column_selector_header_option(truncate(page.title)))
+        options += fields.collect do |f|
+          column_selector_question_option(f, selected)
+        end
+      end
     end
-    options.push(column_selector_option("ID", "id", selected, :style => meta_option_style))
     return options.join("\n")
   end
   

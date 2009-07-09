35fde8a25ccd563e79dce54cc63cce19b3f3a2a8
Nat Budin <natbudin@gmail.com>
2009-07-09 14:09:15 -0400

    Only run the AeUsersLocalTables migration if we don't have legacy tables already

--- db/migrate/20090523171744_ae_users_local_tables.rb
+++ db/migrate/20090523171744_ae_users_local_tables.rb
@@ -1,21 +1,25 @@
 class AeUsersLocalTables < ActiveRecord::Migration
   def self.up
-    create_table :permissions do |t|
-      t.column :role_id, :integer
-      t.column :person_id, :integer
-      t.column :permission, :string
-      t.column :permissioned_id, :integer
-      t.column :permissioned_type, :string
+    unless table_exists? :permissions
+      create_table :permissions do |t|
+        t.column :role_id, :integer
+        t.column :person_id, :integer
+        t.column :permission, :string
+        t.column :permissioned_id, :integer
+        t.column :permissioned_type, :string
+      end
     end
     
-    create_table :auth_tickets do |t|
-      t.column :secret, :string
-      t.column :person_id, :integer
-      t.timestamps
-      t.column :expires_at, :datetime
+    unless table_exists? :auth_tickets
+      create_table :auth_tickets do |t|
+        t.column :secret, :string
+        t.column :person_id, :integer
+        t.timestamps
+        t.column :expires_at, :datetime
+      end
+  
+      add_index :auth_tickets, :secret, :unique => true
     end
-
-    add_index :auth_tickets, :secret, :unique => true
   end
 
   def self.down

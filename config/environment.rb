f08fddb508c81458ac6ff9d1b0babbecff86ecec
Nat Budin <natbudin@gmail.com>
2009-07-09 13:36:46 -0400

    Visual improvements

--- config/environment.rb
+++ config/environment.rb
@@ -38,5 +38,8 @@ Mime::Type.register "image/png", :png
 
 Journey::UserOptions.add_logged_out_option("Log in", {:controller => "auth", :action => "login" })
 Journey::UserOptions.add_logged_out_option("Sign up", {:controller => "account", :action => "signup" })
+
 Journey::UserOptions.add_logged_in_option("Profile", {:controller => "account", :action => "edit_profile" })
+Journey::UserOptions.add_logged_in_option("Admin", {:controller => "permission", :action => "admin" },
+                                          :conditional => lambda { logged_in? and logged_in_person.administrator? })
 Journey::UserOptions.add_logged_in_option("Log out", {:controller => "auth", :action => "logout" })

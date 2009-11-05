c04ec51f25b8dd1c5e3425b7fa8b2fafdb141fb2
Nat Budin <natbudin@gmail.com>
2009-11-05 17:02:32 -0500

    Use absolute image URL

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
     end
     title << "Journey"
   end
+  
+  # by Rob Biedenharn: http://www.mail-archive.com/rubyonrails-talk@googlegroups.com/msg15305.html
+  def image_url(source)
+    abs_path = image_path(source)
+    unless abs_path =~ /\Ahttp/
+      abs_path = "http#{'s' if request.ssl?}://#{request.host_with_port}#{abs_path}"
+    end
+    abs_path
+  end
 end

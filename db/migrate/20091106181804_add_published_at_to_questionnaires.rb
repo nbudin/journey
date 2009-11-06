e331eae258e3aa4e0c07702885574d0216511aab
Nat Budin <natbudin@gmail.com>
2009-11-06 13:40:49 -0500

    oops, actually include the published_at migration

--- db/migrate/20091106181804_add_published_at_to_questionnaires.rb
+++ db/migrate/20091106181804_add_published_at_to_questionnaires.rb
@@ -1,7 +1,13 @@
 class AddPublishedAtToQuestionnaires < ActiveRecord::Migration
   def self.up
+    add_column :questionnaires, :published_at, :timestamp
+    Questionnaire.all(:conditions => { :is_open => true }).each do |q|
+      q.published_at = q.created_at
+      q.save :validate => false
+    end
   end
 
   def self.down
+    remove_column :questionnaires, :published_at
   end
 end

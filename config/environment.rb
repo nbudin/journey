f869c061cb889b1cf9972559700f3ff9ddefda55
Nat Budin <nbudin@kenichi-2.(none)>
2009-12-24 15:36:27 -0500

    Don't get rid of action_mailer, you moron

--- config/environment.rb
+++ config/environment.rb
@@ -13,8 +13,6 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence those specified here
   
-  config.frameworks -= [:action_mailer, :active_resource]
-  
   config.action_controller.session = {
       :key         => '_journey_aegames_org-trunk_session',
       :secret      => 'ca837e0d9bfed2129139ac1712cf768687981f043aca55c15b424d1deee830babca9bf84afea63d9fd18164f9b026b5c5846b3cb1d64d8febeab9618d24a385f',

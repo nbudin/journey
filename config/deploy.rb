fe69373005987c6b45d195f2dcb501eb60da2fbb
Nat Budin <natbudin@gmail.com>
2010-06-29 11:17:11 -0400

    Revert "Deployment updates"
    This reverts commit 84220f89ca3b9fec6fe3bee0af1c7d201ab956e9.

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,8 @@ end
 after "deploy:update_code" do
   bundler.bundle_new_release
   run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
-  run "ln -nfs #{deploy_to}/#{shared_dir}/config/unicorn.rb #{release_path}/config/unicorn.rb"
+  run "rm -f #{release_path}/config/newrelic.yml"
+  run "ln -nfs #{deploy_to}/#{shared_dir}/config/newrelic.yml #{release_path}/config/newrelic.yml"
 end
   
 after "deploy:migrate", "deploy:migrate_paywall"

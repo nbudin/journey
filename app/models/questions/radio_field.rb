7692c18e5d0d5a7d8cc0a576ade88362d4865ba9
Nat Budin <natbudin@gmail.com>
2011-02-09 17:42:48 -0500

    Add vertical radio button layout

--- app/models/questions/radio_field.rb
+++ app/models/questions/radio_field.rb
@@ -2,4 +2,16 @@ class Questions::RadioField < Questions::SelectorField
   def self.friendly_name
     "Radio buttons"
   end
+  
+  unless column_names.include?("radio_layout")
+    class_eval <<-END
+    def radio_layout
+      "inline"
+    end
+    END
+  end
+  
+  def radio_layout_class
+    "layout-#{radio_layout}"
+  end
 end
--- public/javascripts/questionnaireedit.js
+++ public/javascripts/questionnaireedit.js
@@ -266,6 +266,19 @@ function setLayout(questionId, layout) {
   }
 }
 
+function setRadioLayout(questionId, layout) {
+  if (layout == null) {
+    alert('Multiple layout selection not yet implemented.');
+  } else {
+    Question.find(questionId, function(q) {
+      q.radio_layout = layout;
+      q.save(function() {
+        reloadQuestion(questionId);
+      });
+    });
+  }
+}
+
 function toggleDropdown(questionId) {
   //dropdownicon = $('dropdown_icon_'+questionId)
   /*$$('.selected_dropdown_icon').each(function(ddi) {

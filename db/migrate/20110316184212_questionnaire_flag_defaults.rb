b9fbe17e5e13b35b2ae3c9dd051e43387733b257
Nat Budin <natbudin@gmail.com>
2011-03-16 14:49:05 -0400

    Change questionnaire flags to explicit defaults

---
+++ db/migrate/20110316184212_questionnaire_flag_defaults.rb
@@ -0,0 +1,20 @@
+class QuestionnaireFlagDefaults < ActiveRecord::Migration
+  def self.up
+    no_preview = Questionnaire.all(:conditions => { :allow_preview => false }).each
+
+    change_column :questionnaires, :allow_preview, :boolean, :null => false, :default => true
+    transaction do
+      no_preview.each do |q|
+        q.allow_preview = false
+        q.save(false)
+      end
+    end
+    
+    change_column :questionnaires, :allow_delete_responses, :boolean, :null => false, :default => false
+  end
+
+  def self.down
+    change_column :questionnaires, :allow_preview, :boolean, :null => true, :default => nil
+    change_column :questionnaires, :allow_delete_responses, :boolean, :null => true, :default => nil
+  end
+end

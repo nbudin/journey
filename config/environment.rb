8fae4b1377b9e7d0294181a1bda984bda9c23524
Nat Budin <natbudin@gmail.com>
2011-05-18 14:14:41 -0400

    Use secret token from env

--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,7 @@ Rails::Initializer.run do |config|
 
   config.action_controller.session = {
       :key         => '_journey_aegames_org-trunk_session',
-      :secret      => 'ca837e0d9bfed2129139ac1712cf768687981f043aca55c15b424d1deee830babca9bf84afea63d9fd18164f9b026b5c5846b3cb1d64d8febeab9618d24a385f',
+      :secret      => (ENV['SECRET_TOKEN'] || 'ca837e0d9bfed2129139ac1712cf768687981f043aca55c15b424d1deee830babca9bf84afea63d9fd18164f9b026b5c5846b3cb1d64d8febeab9618d24a385f'),
       :expire_after => (2 * 60 * 60 * 24 * 7)
     }
       

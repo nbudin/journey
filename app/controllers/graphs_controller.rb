2680bed79bd7670041c231c7adfea2bf9e9be6a6
Nat Budin <natbudin@gmail.com>
2012-02-01 18:15:10 -0500

    Fix graphs

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -4,19 +4,16 @@ class GraphsController < ApplicationController
   before_filter :set_geom
   
   def line
-    @questions = @questionnaire.questions.all(:conditions => { :id => params[:question_ids] })
-    @counts = {}
+    @questions = Question.all(:conditions => { :id => params[:question_ids] })
+    @counts = aggregate_questions(params[:question_ids])
     @min = @questions.collect { |q| q.min }.min
     @max = @questions.collect { |q| q.max }.max
-    @questions.each do |question|
-      @counts[question] = aggregate_question(question)
-    end
     render :layout => false
   end
   
   def pie
-    @question = @questionnaire.questions.find(params[:question_id])
-    @answercounts = aggregate_question(@question)
+    @answercounts = aggregate_questions(params[:question_id]).values.first
+    @question = Question.find(params[:question_id])
     render :layout => false
   end
   
@@ -29,17 +26,33 @@ class GraphsController < ApplicationController
     @questionnaire = Questionnaire.find(params[:questionnaire_id])
   end
   
-  def aggregate_question(question)
+  def aggregate_questions(question_ids)
+    db = RailsSequel.connect
+          
+    ds = db[:answers]
+    ds = ds.inner_join(:questions, :id => :answers__question_id)
+    ds = ds.inner_join(:pages, :id => :questions__page_id)
+    ds = ds.left_join(:question_options, :question_id => :answers__question_id, :option => :answers__value)
+    ds = ds.where(:questions__id => question_ids)
+    ds = ds.where(:pages__questionnaire_id => @questionnaire.id)
+    ds = ds.select(:questions__id, :answers__value, :question_options__output_value)
+    
     counts = {}
-    question.answers.each do |answer|
-      value = answer.output_value
-      counts[value] ||= 0
-      counts[value] += 1
+    ds.each do |db_row|
+      question_id = db_row[:id]
+      value = (db_row[:output_value] || db_row[:value] || "")
+      counts[question_id] ||= {}
+      counts[question_id][value] ||= 0
+      counts[question_id][value] += 1
     end
-    no_answer = @questionnaire.responses.valid.no_answer_for(question).count()
-    if no_answer > 0
-      counts["No answer"] = no_answer
+    
+    Question.all(:conditions => { :id => question_ids }).each do |question|
+      no_answer = @questionnaire.responses.valid.no_answer_for(question).count()
+      if no_answer > 0
+        counts[question.id]["No answer"] = no_answer
+      end
     end
+    
     return counts
   end
 end

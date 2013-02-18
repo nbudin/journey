882593913c210cccab88dbf79d508f199424e026
Nat Budin <nbudin@patientslikeme.com>
2013-02-18 08:30:16 -0500

    Syntax fix

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -6,7 +6,7 @@ else
   ENV['SECRET_TOKEN'] || 'ca837e0d9bfed2129139ac1712cf768687981f043aca55c15b424d1deee830babca9bf84afea63d9fd18164f9b026b5c5846b3cb1d64d8febeab9618d24a385f'
 end
 
-config.action_controller.session = {
+Rails.configuration.action_controller.session = {
     :key         => '_journey_aegames_org-trunk_session',
     :secret      => secret_token,
     :expire_after => 2.weeks

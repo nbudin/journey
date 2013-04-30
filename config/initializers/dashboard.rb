6f5352a9aa3e6b0a7c9f2dd8633a6f20c7880190
Nat Budin <nbudin@patientslikeme.com>
2013-04-30 08:35:44 -0700

    Fix initializers for Rails 3

--- config/initializers/dashboard.rb
+++ config/initializers/dashboard.rb
@@ -1,3 +1,5 @@
+require 'journey_questionnaire'
+
 Journey::Dashboard.add_dashbox("new_surveys", :left)
 Journey::Dashboard.add_dashbox("my_responses", :left)
 Journey::Dashboard.add_dashbox("my_surveys", :right, :top)
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,9 +1,4 @@
 require 'journey_config'
 
-secret_token = JourneyConfig.config['secret_token'] || 'ca837e0d9bfed2129139ac1712cf768687981f043aca55c15b424d1deee830babca9bf84afea63d9fd18164f9b026b5c5846b3cb1d64d8febeab9618d24a385f'
-
-ActionController::Base.session = {
-    :key         => '_journey_aegames_org-trunk_session',
-    :secret      => secret_token,
-    :expire_after => 2.weeks
-  }
+Rails.application.config.session_store :cookie_store, :key => '_journey_aegames_org-trunk_session'
+Rails.application.config.secret_token = JourneyConfig.config['secret_token'] || 'ca837e0d9bfed2129139ac1712cf768687981f043aca55c15b424d1deee830babca9bf84afea63d9fd18164f9b026b5c5846b3cb1d64d8febeab9618d24a385f'

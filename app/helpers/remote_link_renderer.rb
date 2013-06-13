edf0cfd46d022ddefc95f2729f7989b0242a0cff
Nat Budin <nbudin@patientslikeme.com>
2013-06-13 13:54:19 -0400

    Port the link renderer to new will_paginate version

--- app/helpers/remote_link_renderer.rb
+++ app/helpers/remote_link_renderer.rb
@@ -1,15 +1,11 @@
-# app/helpers/remote_link_renderer.rb
-# modified from http://weblog.redlinesoftware.com/2008/1/30/willpaginate-and-remote-links
-
-class RemoteLinkRenderer < WillPaginate::LinkRenderer
+class RemoteLinkRenderer < WillPaginate::ViewHelpers::LinkRenderer
   def prepare(collection, options, template)
     @remote = options.delete(:remote) || {}
     super
   end
 
 protected
-  def page_link(page, text, attributes = {})
-    @template.link_to_remote(text, {:url => url_for(page), :method => :get}.merge(@remote), 
-                             { :href => url_for(page) })
+  def link(text, target, attributes = {})
+    super(text, target, attributes.merge(remote: @remote, method: :get))
   end
 end

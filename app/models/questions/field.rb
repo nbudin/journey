f5fe77134d677157a795f7e8e1d74c762650795e
Nat Budin <nbudin@patientslikeme.com>
2013-06-24 13:17:30 -0400

    These are already defined (better) in the parent class

--- app/models/questions/field.rb
+++ app/models/questions/field.rb
@@ -1,37 +1,6 @@
 class Questions::Field < Question
   has_one :special_field_association, :foreign_key => :question_id
   
-  def purpose
-    if special_field_association.nil?
-      nil
-    else
-      special_field_association.purpose
-    end
-  end
-  
-  def purpose=(newpurpose)
-    if not (newpurpose.nil? or newpurpose == '')
-      if special_field_association.nil?
-        SpecialFieldAssociation.create!(:questionnaire => questionnaire,
-                                        :purpose => newpurpose,
-                                        :question => self)
-        reload
-      else
-        special_field_association.purpose = newpurpose
-      end
-    else
-      if not special_field_association.nil?
-        special_field_association.destroy
-      end
-    end
-  end
-  
-  before_save do |field|
-    if not field.special_field_association.nil?
-      field.special_field_association.save!
-    end
-  end
-  
   def to_json
     super :methods => "purpose"
   end

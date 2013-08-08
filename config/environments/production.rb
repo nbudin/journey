a3a45c77d65786f44a363b445c6dfb599a4987c8
Nat Budin <nbudin@patientslikeme.com>
2013-08-08 15:03:39 -0400

    Switch to sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,12 +59,7 @@ Journey::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "localhost",
-    :port => 25,
-    :domain => "journeysurveys.com"
-  }
+  config.action_mailer.delivery_method = :sendmail
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true

1dd8bb5911a9a78c3a94a25f3f5a3f888c51df5e
Nat Budin <nbudin@patientslikeme.com>
2014-02-21 16:53:49 -0800

    Don't break if there's no question (although that's a problem)

--- db/migrate/20140221233548_fix_the_fucking_questionnaire_ids_in_special_field_associations.rb
+++ db/migrate/20140221233548_fix_the_fucking_questionnaire_ids_in_special_field_associations.rb
@@ -1,6 +1,8 @@
 class FixTheFuckingQuestionnaireIdsInSpecialFieldAssociations < ActiveRecord::Migration
   def up
     SpecialFieldAssociation.includes(:questionnaire, question: { page: :questionnaire }).find_each do |sfa|
+      next unless sfa.question
+      
       sfa.questionnaire = sfa.question.questionnaire
       sfa.save!
     end

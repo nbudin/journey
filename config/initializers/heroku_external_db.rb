2ee6bdd78833f81bbc280acb8ae3e91312664f37
Nat Budin <nbudin@patientslikeme.com>
2014-06-08 08:19:51 -0700

    Get rid of heroku_external_db

--- config/initializers/heroku_external_db.rb
+++
@@ -1,6 +0,0 @@
-if ENV['EXTERNAL_DATABASE_URL']
-  HerokuExternalDb.setup_configuration!("USERS", "users")
-
-  # This has to come last so that it will be the default ActiveRecord configuration.
-  HerokuExternalDb.setup_rails_env!
-end

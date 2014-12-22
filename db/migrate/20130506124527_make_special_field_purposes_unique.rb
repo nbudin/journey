9f9b6f7c89f66dc1794cdfa86e966ce90220e043
Nat Budin <nbudin@patientslikeme.com>
2014-12-22 10:10:14 -0500

    Work around too-long index name

--- db/migrate/20130506124527_make_special_field_purposes_unique.rb
+++ db/migrate/20130506124527_make_special_field_purposes_unique.rb
@@ -1,9 +1,9 @@
 class MakeSpecialFieldPurposesUnique < ActiveRecord::Migration
   def self.up
-    add_index :special_field_associations, [:questionnaire_id, :purpose], :unique => true
+    add_index :special_field_associations, [:questionnaire_id, :purpose], :unique => true, name: 'idx_special_field_associations_unique'
   end
 
   def self.down
-    remove_index :special_field_associations, [:questionnaire_id, :purpose]
+    remove_index :special_field_associations, name: 'idx_special_field_associations_unique'
   end
 end

db00245d82e46445399f9c57e435c714998e341b
Nat Budin <nbudin@patientslikeme.com>
2015-01-03 09:02:56 -0500

    Allow publish settings

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -231,6 +231,7 @@ class QuestionnairesController < ApplicationController
   
   def permitted_params_for_edit_permission
     [:title, :is_open, :custom_html, :custom_css, :allow_finish_later, :allow_amend_response, 
-      :welcome_text, :advertise_login, :require_login, :publicly_visible, :allow_preview, :allow_delete_responses]
+      :welcome_text, :advertise_login, :require_login, :publicly_visible, :allow_preview, :allow_delete_responses,
+      :publication_mode, :login_policy]
   end
 end

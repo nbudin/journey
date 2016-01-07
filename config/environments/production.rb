0a96bdd8ad156d321ce6838a805672249b5b22e3
Nat Budin <nbudin@patientslikeme.com>
2016-01-07 16:27:47 -0500

    Enable cache busting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Journey::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  
+
   # Enfore SSL in prod
   config.middleware.insert_before ActionDispatch::Static, ::Rack::SSL, :host => "secure.journeysurveys.com"
 
@@ -17,6 +17,7 @@ Journey::Application.configure do
 
   # Compress both stylesheets and JavaScripts
   config.assets.compress = true
+  config.assets.digest = true
 
   # Specifies the header that your server uses for sending files
   # (comment out if your front-end server doesn't support this)
@@ -36,7 +37,7 @@ Journey::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   #config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
-  
+
   config.assets.digest = false
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
@@ -44,7 +45,7 @@ Journey::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  
+
   config.action_mailer.default_url_options = {
     host: "secure.journeysurveys.com",
     protocol: "https"
@@ -59,12 +60,12 @@ Journey::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  
+
   config.action_mailer.delivery_method = :sendmail
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  
+
   config.action_mailer.asset_host = "https://secure.journeysurveys.com"
 end
 

86cffa7dcc672169915fe55ef3a1dd7d44058e55
Nat Budin <nbudin@patientslikeme.com>
2016-03-08 22:56:34 -0500

    Make it work for radios too

--- app/assets/javascripts/other_value.js
+++ app/assets/javascripts/other_value.js
@@ -1,4 +1,30 @@
-var toggleOtherValueVisibility = function(element) {
+var toggleOtherValueVisibilityForRadio = function(element) {
+  var otherOptions = element.up('ul').select('input[type=radio]').filter(function (childElement) {
+    return childElement.readAttribute("data-is-other");
+  });
+
+  if (otherOptions.length > 0) {
+    var otherValues = otherOptions.map(function (otherOption) { return otherOption.readAttribute("value") });
+    var selectedRadios = $$('input:checked[type=radio][name='+element.readAttribute('name')+']');
+    var selectValue;
+    if (selectedRadios.length > 0) {
+      selectValue = selectedRadios[0].value;
+    }
+    var otherSelected = (otherValues.indexOf(selectValue) != -1);
+
+    var questionId = element.readAttribute('data-question-id');
+    var textField = $$('input[data-other-response-for-question-id=' + questionId + ']')[0];
+
+    if (otherSelected) {
+      textField.show();
+    } else {
+      textField.hide();
+    }
+  }
+};
+
+
+var toggleOtherValueVisibilityForSelect = function(element) {
   var otherOptions = element.childElements().filter(function (childElement) {
     return childElement.readAttribute("data-is-other");
   });
@@ -20,8 +46,13 @@ var toggleOtherValueVisibility = function(element) {
 };
 
 document.observe('dom:loaded', function() {
-  $$('select[data-question-id]').each(function(selector) {
-    selector.on('change', function (event) { toggleOtherValueVisibility(event.target); });
-    toggleOtherValueVisibility(selector);
+  $$('select[data-question-id]').each(function(select) {
+    select.on('change', function (event) { toggleOtherValueVisibilityForSelect(event.target); });
+    toggleOtherValueVisibilityForSelect(select);
+  });
+
+  $$('input[type=radio][data-question-id]').each(function(radio) {
+    radio.on('change', function (event) { toggleOtherValueVisibilityForRadio(event.target); });
+    toggleOtherValueVisibilityForRadio(radio);
   });
 });
